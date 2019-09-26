ce59abf79291fdbf0cd3fa58dbbeb389e66b9a49
Jackson Oppenheim <jacksonopp@gmail.com>
2019-09-26 15:58:14 -0500

    added seeds and updated title in head:

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,2 +1,32 @@
-into Burgers (burger_name)
-insert values ("pig lebowski");
+use burger_db;
+
+select * from burgers;
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("pig lebowski","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("Itsy Bitsy Teeny Weenie Yellow Polka-Dot Zucchini Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("Summer Thyme Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("Olive and Let Die Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("I've Created a Muenster Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("Beets of the Southern Wild Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("A Leek of Their Own Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("Kales From the Crypt Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("Open Sesame Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
+
+insert into Burgers (burger_name, createdAt, updatedAt)
+values ("The Gourdon-Hamsey Burger","2019-09-26 19:51:08","2019-09-26 19:51:08");
